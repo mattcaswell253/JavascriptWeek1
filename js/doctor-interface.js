ff497831be23f445e2b41536912583943426f369
mattcaswell <matthewcaswell253@Gmail.com>
2017-03-17 15:15:41 -0700

    too frusterated to accomplish anything

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,17 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-
+var displayDoctor = function(name) {
+  $('#solution').append("<li>Doctors name:" + name + "<li>" );
+};
 
 $(document).ready(function() {
   var newDoctor = new Doctor();
+
   $('#doctor-form').submit(function(event) {
     event.preventDefault();
     var issue = $('#issue').val();
-    $('issue').val("");
-    newDoctor.getDoctors(issue)
+    newDoctor.getDoctors(issue);
+    $('#issue').val("");
+
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,15 +4,29 @@ function Doctor() {
 
 }
 
+var name = "";
+
+
 Doctor.prototype.getDoctors = function(medicalIssue) {
+  doctorArray = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
+   .then(function(response) {
+
+    //  for (var i = 0; i < response.data.length; i++);
+    //  firstName = response.data[i].profile['first_name'];
+    //  lastName = response.data[i].profile['last_name'];
+
+
+      response.data.forEach(function(element) {
+        name = (element.profile.first_name + " " + element.profile.last_name );
+        console.log(name);
+
+
+        // displayDoctor(element.profile.first_name, element.profile.last_name);
+
+    }).fail(function(error){
       $('.error-message').text(error.responseJSON.message);
     });
+  });
 };
-
 exports.doctorModule = Doctor;
